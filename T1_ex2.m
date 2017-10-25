8402cc276b07e6679a99e9c36bb517ea3a92613c
TheLonelyFighter <32803079+TheLonelyFighter@users.noreply.github.com>
2017-10-25 21:51:32 +0300

    Update T1_ex2.m

--- T1_ex2.m
+++ T1_ex2.m
@@ -1,7 +1,17 @@
 t=0:0.002:10;
-s=1.5*sawtooth(2*pi*0.2*t,0.6)-0.5 ; 
-plot(t,s),grid
-%am folosit functia sawtooth pt generarea semnalului triunghiular
+s=1.5*sawtooth(2*pi*0.2*t,0.6)-0.5; %am folosit functia sawtooth pt generarea semnalului triunghiular
 %pentru care am ajustat amplitudinea si componenta continua;
 %al doilea parametru al functiei este folosit pentru a regla panta dreptei
 %semnalului
+figure(1)
+plot(t,s),grid
+
+t=0:0.02:10; %am modificat pasul la 20 ms
+s=1.5*sawtooth(2*pi*0.2*t,0.6)-0.5;
+figure(2)
+plot(t,s),grid
+
+t=0:0.2:10; %am modificat pasul la 200 ms
+s=1.5*sawtooth(2*pi*0.2*t,0.6)-0.5;
+figure(3)
+plot(t,s),grid

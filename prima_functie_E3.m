406826d90bb4fb3a5df09224303d221b46ded2d2
TheLonelyFighter <32803079+TheLonelyFighter@users.noreply.github.com>
2017-10-30 19:44:33 +0200

    Update prima_functie_E3.m

--- prima_functie_E3.m
+++ prima_functie_E3.m
@@ -1,4 +1,4 @@
 function [media,vector_2,matrice_2]= prima_functie(z)
 media=mean(real(z))
-vector=z.^2
-matrice=z*(z.')
+vector=z.^2    %ridica la patrat toate elementele matricei
+matrice=z*(z.')  %inmultesc matricea z cu transpusa ei
